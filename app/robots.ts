aa8c3ccafc9687518a342c070556942a34417f8e
SL_NOTEBOOK\tough <admin@jedaeroweb.co.kr>
2026-07-01 21:10:53 +0900

    modified:   app/robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,41 +1,11 @@
-import type { Metadata } from "next";
+import type { MetadataRoute } from "next";
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://www.jedaeroweb.co.kr"),
-
-  title: {
-    default: "Conveyor Game",
-    template: "%s | Conveyor Game",
-  },
-
-  description: "Conveyor puzzle game",
-
-  applicationName: "Conveyor Game",
-
-  keywords: [
-    "game",
-    "conveyor",
-    "puzzle",
-    "sorting",
-    "brain game",
-  ],
-
-  authors: [
-    {
-      name: "Jedaeroweb",
-      url: "https://www.jedaeroweb.co.kr",
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
     },
-  ],
-
-  creator: "Jedaeroweb",
-
-  openGraph: {
-    type: "website",
-    siteName: "Conveyor Game",
-    locale: "ko_KR",
-  },
-
-  twitter: {
-    card: "summary_large_image",
-  },
-};
+    sitemap: "https://example.com/sitemap.xml",
+  };
+}
